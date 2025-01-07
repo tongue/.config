219b1289afe86e3f97fdaf60ee1a5a2088b8e5fa
Johan Bergström <johan.bergstrom@alster.se>
2025-01-07 16:14:01 +0100

    stuff i missed

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,3 +7,5 @@ vim.keymap.set("n", "[q", "<cmd>cprev<CR>", { desc = "[Q]uickfix Previous" })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+
+vim.keymap.set("n", "<esc><esc>", "<cmd>nohl<CR>", { desc = "Clear highlight" })
